3e4731cb4d5bb3dcc01a81490ebdc973458397b8
Jeel Dobariya <jeeldobariya38@gmail.com>
2026-07-29 16:32:05 +0530

    Update

--- Java/P2.java
+++ Java/P2.java
@@ -6,9 +6,8 @@ public class P2 {
         Class.forName("com.mysql.cj.jdbc.Driver");
 
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee", "root", "");
-        setupDB(conn);
-
         Scanner sc = new Scanner(System.in);
+ 
         System.out.print("Enter id: ");
         int id = sc.nextInt();
 
@@ -22,43 +21,5 @@ public class P2 {
 
         sc.close();
         conn.close();
-    }
-
-    private static void setupDB(Connection conn) throws SQLException {
-        Statement st = conn.createStatement();
-
-        // 1. Create table
-        st.execute(
-            """
-            CREATE TABLE IF NOT EXISTS emp (
-                id INTEGER AUTO_INCREMENT PRIMARY KEY, 
-                name VARCHAR(50), 
-                age INTEGER, 
-                city VARCHAR(50)
-            );
-            """
-        );
-
-        // 2. Drop stored procedure if it already exists
-        st.execute("DROP PROCEDURE IF EXISTS get_city");
-
-        // 3. Create stored procedure (No DELIMITER keywords needed)
-        st.execute(
-            """
-            DELIMITER //
-
-            CREATE PROCEDURE get_city(
-                IN p_empid INT,
-                OUT p_city VARCHAR(50)
-            )
-            BEGIN
-                SELECT city INTO p_city 
-                FROM emp 
-                WHERE id = p_empid;
-            END //
-
-            DELIMITER ;
-            """
-        );
     }
 }
