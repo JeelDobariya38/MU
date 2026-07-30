6d6ce2baf15049d3661d77e070e19571fc048157
Jeel Dobariya <jeeldobariya38@gmail.com>
2026-07-30 13:38:58 +0530

    Update

--- Java/P2.java
+++ Java/P2.java
@@ -7,17 +7,18 @@ public class P2 {
 
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee", "root", "");
         Scanner sc = new Scanner(System.in);
- 
+
         System.out.print("Enter id: ");
         int id = sc.nextInt();
 
-        CallableStatement cst = conn.prepareCall("");
+        CallableStatement cst = conn.prepareCall("{call get_city(?,?)}");
 
         cst.setInt(1, id);
         cst.registerOutParameter(2, Types.VARCHAR);
         cst.execute();
 
         System.out.println("ID=" + id + ", City=" + cst.getString(2));
+        System.out.println();
 
         sc.close();
         conn.close();
